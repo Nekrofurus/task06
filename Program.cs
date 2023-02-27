1173389edf11700ba46dea059f62cba40925ed81
VVCephei <vladgol90@gmail.com>
2023-02-27 12:19:53 +0200

    Задача 6

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Напишите программу, которая на вход принимает число и выдает, является ли число чётным
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+Console.WriteLine("Да, четное");
+else
+Console.WriteLine("Нет, нечетное");
